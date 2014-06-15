1a7c96b8b865f59361619cf5e35045639c971a81
RIscRIpt <richard@riscript.com>
2014-06-15 17:45:24 +0300

    Fastrun: moving in all directions support

--- clientdll.asm
+++ clientdll.asm
@@ -36,28 +36,86 @@ proc CL_CreateMove c frametime, cmd, active
 	feature FASTRUN
 		bt [userButtons], UB_FASTRUN
 		jnc .end_FASTRUN
-			;TODO: Add multi side support
+			test [.pmove.flags], FL_ONGROUND
+			jz .end_FASTRUN
+			
+			mov eax, dword[.cmd.buttons]
+			test eax, IN_FORWARD
+			jz .fr_fwd_bck_ok
+				test eax, IN_BACK
+				jz .fr_fwd_bck_ok
+					and eax, not (IN_FORWARD or IN_BACK)
+			.fr_fwd_bck_ok:
+			test eax, IN_MOVELEFT
+			jz .fr_lft_rgt_ok
+				test eax, IN_MOVERIGHT
+				jz .fr_lft_rgt_ok
+					and eax, not (IN_MOVELEFT or IN_MOVERIGHT)
+			.fr_lft_rgt_ok:
+			
+			test eax, IN_FORWARD
+			jnz .fr_fwd
+			test eax, IN_BACK
+			jnz .fr_bck
+			test eax, IN_MOVERIGHT
+			jnz .fr_rgt
+			test eax, IN_MOVELEFT
+			;jnz .fr_lft
+			;jmp .end_FASTRUN
+			jz .end_FASTRUN
+			.fr_lft:
+				mov [fastrun_movement_angle], 1.5707963267948966192313216916398 ;90.0 * cDEG_TO_RAD
+				jmp .fr_begin
+			.fr_rgt:
+				mov [fastrun_movement_angle], -1.5707963267948966192313216916398 ;-90.0 * cDEG_TO_RAD
+				jmp .fr_begin
+			.fr_fwd:
+				test eax, IN_MOVERIGHT
+				jnz .fr_fwd_rgt
+				test eax, IN_MOVELEFT
+				jnz .fr_fwd_lft
+					mov [fastrun_movement_angle], 0.0 ;0.0 * cDEG_TO_RAD
+					jmp .fr_begin
+				.fr_fwd_rgt:
+					mov [fastrun_movement_angle], -0.78539816339744830961566084582005 ;-45.0 * cDEG_TO_RAD
+					jmp .fr_begin
+				.fr_fwd_lft:
+					mov [fastrun_movement_angle], 0.78539816339744830961566084582005 ;45.0 * cDEG_TO_RAD
+					jmp .fr_begin
+			.fr_bck:
+				test eax, IN_MOVELEFT
+				jnz .fr_bck_lft
+				test eax, IN_MOVERIGHT
+				jnz .fr_bck_rgt
+					mov [fastrun_movement_angle], 3.1415926535897932384626433832795 ;180.0 * cDEG_TO_RAD
+					jmp .fr_begin
+				.fr_bck_lft:
+					mov [fastrun_movement_angle], 2.3561944901923449288469825374596 ;135.0 * cDEG_TO_RAD
+					jmp .fr_begin
+				.fr_bck_rgt:
+					mov [fastrun_movement_angle], -2.3561944901923449288469825374596 ;-135.0 * cDEG_TO_RAD
+					;jmp .fr_begin
+
+			.fr_begin:
+			fld [.cmd.viewangles.y]
+			fmul [flDEG_TO_RAD]
+			
 			fld [.pmove.velocity.y]
 			fld [.pmove.velocity.x]
 			fpatan
 			;ST0 = atan2(.pmove.velocity.y, .pmove.velocity.x)
-			
-			fld [.cmd.viewangles.y]
-			fmul [flDEG_TO_RAD]
+			fsub [fastrun_movement_angle]
 			fsubp ST1, ST0
 			;ST0 = difference between movement angle and view angle
-			fst ST1
+			fst [fastrun_angle_diff_flt]
 			fmul [flRAD_TO_DEG]
-			fistp [fastrun_angle_diff_int]		
-			fsincos
-			;ST0 = cos(angle diff)
-			;ST1 = sin(angle diff)
+			fistp [fastrun_angle_diff_int]
+
 			fld [fastrun_sidemove.value]
 			
 			mov ecx, 360
 			xor edx, edx
 			mov eax, [fastrun_angle_diff_int]
-			neg eax
 			add eax, ecx
 			div ecx
 			shr ecx, 1
@@ -72,30 +130,57 @@ proc CL_CreateMove c frametime, cmd, active
 			fcmovb ST0, ST2
 			fld [fastrun_forwardmove.value]
 			
+			fld [fastrun_movement_angle]
+			fsincos ;FPU: 0cos, 1sin, 2fwd, 3sid, 4fwd, 5sid
 			fldz
-			fcmovb ST0, ST6
+			fcmovb ST0, ST2
 			fldz
-			fcmovb ST0, ST6
+			fcmovb ST0, ST2
+			;ST0 = cos(angle diff)
+			;ST1 = sin(angle diff)
+			;ST2 = cos(angle diff)
+			;ST3 = sin(angle diff)
+			;ST4 = fastrun_forwardmove.value
+			;ST5 = fastrun_sidemove.value
+			;ST6 = fastrun_forwardmove.value
+			;ST7 = fastrun_sidemove.value
+			fmulp ST5, ST0
+			fmulp ST3, ST0
+			fmulp ST4, ST0
+			fmulp ST4, ST0
+			fsubp ST1, ST0
+			fst ST3
+			fstp ST5
+			faddp ST1, ST0
+			fst ST2
+			;ST0,ST2 = fastrun_forwardmove.value * cos(angle diff) + fastrun_sidemove.value * sin(angle diff)
+			;ST1,ST3 = fastrun_sidemove.value * cos(angle diff) - fastrun_forwardmove.value * sin(angle diff)
+			
+			fld [fastrun_angle_diff_flt]
+			fchs
+			fsincos
+			;ST0 = cos(angle diff)
+			;ST1 = sin(angle diff)
+			
+			fldz
+			fcmovb ST0, ST2
+			fldz
+			fcmovb ST0, ST2
 			
 			;ST0 = cos(angle diff)
 			;ST1 = sin(angle diff)
-			;ST2 = fastrun_forwardmove.value
-			;ST3 = fastrun_sidemove.value
-			;ST4 = fastrun_forwardmove.value
-			;ST5 = fastrun_sidemove.value
-			;ST6 = cos(angle diff)
-			;ST7 = sin(angle diff)			
-			fmulp ST3, ST0
-			fmulp ST1, ST0
-			fsubp ST1, ST0
-			fstp [.cmd.sidemove]
-			
-			;ST0 = fastrun_forwardmove.value
-			;ST1 = fastrun_sidemove.value
 			;ST2 = cos(angle diff)
 			;ST3 = sin(angle diff)
-			fmulp ST2, ST0
-			fmulp ST2, ST0
+			;ST4 = fastrun_forwardmove.value
+			;ST5 = fastrun_sidemove.value
+			;ST6 = fastrun_forwardmove.value
+			;ST7 = fastrun_sidemove.value
+			fmulp ST5, ST0
+			fmulp ST3, ST0
+			fmulp ST4, ST0
+			fmulp ST4, ST0
+			fsubp ST1, ST0
+			fstp [.cmd.sidemove]
 			faddp ST1, ST0
 			fstp [.cmd.forwardmove]
 	endf
@@ -103,7 +188,7 @@ proc CL_CreateMove c frametime, cmd, active
 	feature BHOP
 		cmp [bhop.value], 0.0
 		je .end_BHOP
-			test [.cmd.buttons], IN_JUMP
+			test dword[.cmd.buttons], IN_JUMP
 			jz .end_BHOP
 				feature BHOP_STANDUP
 					cmp [bhop_standup.value], 0.0
@@ -113,7 +198,7 @@ proc CL_CreateMove c frametime, cmd, active
 						fcomip ST1
 						fstp ST0
 						jbe .end_BHOP_STANDUP
-							or [.cmd.buttons], IN_DUCK
+							or dword[.cmd.buttons], IN_DUCK
 				endf
 				test [.pmove.flags], FL_ONGROUND
 				jnz .end_BHOP
@@ -121,7 +206,7 @@ proc CL_CreateMove c frametime, cmd, active
 					je .end_BHOP
 						cmp [.pmove.waterlevel], 2
 						jge .end_BHOP
-							and [.cmd.buttons], not IN_JUMP
+							and dword[.cmd.buttons], not IN_JUMP
 	endf
 	
 	feature GROUND_STRAFE
@@ -136,15 +221,18 @@ proc CL_CreateMove c frametime, cmd, active
 					jnz .gs_not_on_ground
 						cmp [.pmove.bInDuck], 0
 						jne .gs_not_on_ground
-							or [.cmd.buttons], IN_DUCK
+							or dword[.cmd.buttons], IN_DUCK
 							jmp .end_GROUND_STRAFE
 				.gs_not_on_ground:
-				and [.cmd.buttons], not IN_DUCK
+				and dword[.cmd.buttons], not IN_DUCK
 	endf
 	
 	feature STRAFE
 		bt [userButtons], UB_STRAFE
 		jnc .end_STRAFE
+			test [.pmove.flags], FL_ONGROUND
+			jnz .end_STRAFE
+			
 			fld [strafe_forwardmove.value]
 			fld1
 			fadd [me.horizontal_speed]
@@ -167,13 +255,13 @@ proc CL_CreateMove c frametime, cmd, active
 				fstp ST0
 				ja .jb_prepare
 					mov_dbl_const clientSpeed, 1.0
-					and [.cmd.buttons], not IN_DUCK
-					or [.cmd.buttons], IN_JUMP
+					and dword[.cmd.buttons], not IN_DUCK
+					or dword[.cmd.buttons], IN_JUMP
 					btr [userButtons], UB_JUMPBUG
 					jmp .end_JUMPBUG
 				.jb_prepare:
-					or [.cmd.buttons], IN_DUCK
-					and [.cmd.buttons], not IN_JUMP
+					or dword[.cmd.buttons], IN_DUCK
+					and dword[.cmd.buttons], not IN_JUMP
 					fld [frametime]
 					fld [.pmove.velocity.z]
 					fmulp ST1, ST0
