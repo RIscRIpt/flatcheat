502752fb7fa66f750eabe673a6e9e92395c22332
RIscRIpt <richard@riscript.com>
2014-07-04 00:03:13 +0300

    Fix dynapi for WinXP

--- src/dynamic_api/dynwinapi.asm
+++ src/dynamic_api/dynwinapi.asm
@@ -1,9 +1,4 @@
 proc LoadDynamicAPI
-	invoke GetModuleHandleA, szKernel32DLL
-	test eax, eax
-	jz FatalError
-	mov [hKernel32], eax
-	
 	mov ebx, dynapiList
 	.next:
 	call dword[ebx]
@@ -13,14 +8,39 @@ proc LoadDynamicAPI
 	ret
 endp
 
+proc _DAPI_CheckDLL ;edi - dest, edx - string
+	invoke GetModuleHandleA, edx
+	test eax, eax
+	jz .error
+	mov [edi], eax
+	ret
+	.error:
+	test esi, esi
+	jz FatalError
+	ret
+endp
+
+macro DAPI_CheckDLL dest, s, fail_on_error {
+	mov edi, dest
+	mov edx, s
+	if fail_on_error
+		xor esi, esi
+	else
+		mov esi, 1
+	end if
+	stdcall _DAPI_CheckDLL
+}
+
 proc LDAPI_GetModuleInformation
+	DAPI_CheckDLL hKernel32, szKernel32DLL, 1
 	invoke GetProcAddress, [hKernel32], szK32GetModuleInformation
 	test eax, eax
 	jz .XP
 	mov [GetModuleInformation], eax
 	ret
 	.XP: ;Fuuuuu XP is not be supported anymore!
-	invoke GetProcAddress, [hKernel32], szGetModuleInformation
+	DAPI_CheckDLL hPSAPI, szPsAPIDLL, 1
+	invoke GetProcAddress, [hPSAPI], szGetModuleInformation
 	test eax, eax
 	jz FatalError
 	mov [GetModuleInformation], eax
